3d378d0f8c5f2f39639bc15c67dcacc8da32611c
Jon-Ethan Richards <45516368+JE-Richards@users.noreply.github.com>
2024-05-21 13:32:08 +0100

    Complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // only consider alphanumerics
+    const alphanum = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    // convert input to all lowercase
+    // remove all non-alphanumerics from the input
+    const cleanString = string.toLowerCase().split("").filter((char) => alphanum.includes(char)).join("");
+
+    // reverse string and test
+
+    return cleanString === cleanString.split("").reverse().join("")
 };
 
 // Do not edit below this line
