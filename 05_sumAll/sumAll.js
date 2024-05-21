28510f04c630824edd552e3ef1cb720961813752
Jon-Ethan Richards <45516368+JE-Richards@users.noreply.github.com>
2024-05-21 12:07:21 +0100

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    
+    if ((Math.min(a,b) < 0) || typeof(a) != 'number' || typeof(b) != 'number') {
+        return "ERROR"
+    }
+    else {
+        // Gauss
+        return (((Math.max(a, b) - Math.min(a,b))+1) * (a + b)) / 2;
+    }
 };
 
 // Do not edit below this line
