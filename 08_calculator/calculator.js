8b3c25bd67403cfb055c8e2b8d4334bdbbc39f81
Jon-Ethan Richards <45516368+JE-Richards@users.noreply.github.com>
2024-05-21 13:09:55 +0100

    Complete exercise 08_calculator
    Added an additional test to calculator.spec.js to test what happens when
    factorial is passed a negative value. Coded factorial to return an error
    string when a negative number is passed. The code passed the additional
    test.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, item) => total * item);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = a;
+	if (a < 0) {
+    return "Cannot factorial a negative number";
+  }
+  else if (a === 0 || a === 1) {
+    return 1;
+  }
+  else {
+    while (a > 1) {
+      a--;
+      result = result * a;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
