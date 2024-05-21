d3844fd4e1e5ea30857a0819556bfbd9c13e3126
Jon-Ethan Richards <45516368+JE-Richards@users.noreply.github.com>
2024-05-21 11:38:38 +0100

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const newArray = [];
 
+    // for each item in the array, push it to a new array if it's not included in the function arguments
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    })
+
+    return newArray
 };
 
 // Do not edit below this line
