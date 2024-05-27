4a775fb7fe6e4b8aeeacb141a22032923285b8f5
Jon-Ethan Richards <45516368+JE-Richards@users.noreply.github.com>
2024-05-27 13:23:36 +0100

    Complete exercise 11_getTheTitles
    My solution gets the desired result, but is more involved than the model
    solution provided in the solution subfolder.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let titlesArr = [];
+    arr.forEach(element => {
+        titlesArr.push(element['title']);
+    });
+    return titlesArr;
 };
 
 // Do not edit below this line
